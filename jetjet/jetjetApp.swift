d5492148e14f14564c8b035c453733fb88b53a37
geek <geek.geunbon@gmail.com>
2024-08-06 21:37:13 +0900

    first Commit

---
+++ jetjet/jetjetApp.swift
@@ -0,0 +1,17 @@
+//
+//  jetjetApp.swift
+//  jetjet
+//
+//  Created by geunbon on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct jetjetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
